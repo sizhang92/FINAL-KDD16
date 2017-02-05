91be5cc2f4f460df85eacbf0dfc7cd3e77da9cf2
victorjmarin <victorjmarin@users.noreply.github.com>
2017-02-04 21:17:01 -0500

    Minor fixes

--- run.m
+++ run.m
@@ -4,12 +4,12 @@ load('DBLP-CoAuthor.mat');
 S1 = final_NE(A, B, H, node_label1, node_label2, edge_label1, edge_label2,...
             l_node, l_edge, alpha, 40, 1);
         
-clear;
+clearvars -except S1 perm;
 
 load('flickr-lastfm.mat')
 S2 = final_NE(flickr, lastfm, H, flickr_gender_node_label, lastfm_gender_node_label, ...
             flickr_pagerank_edge_label, lastfm_pagerank_edge_label, l_node, l_edge, ...
-            40, 0);
+            alpha, 40, 0);
 
 % To get the one-to-one mappings, we use greedy matching algorithm
 [M1, ~] = greedy_match(S1);
@@ -19,7 +19,7 @@ accuracy1 = nnz(perm(row) == col')/length(perm);
 [M2, ~] = greedy_match(S2);
 [row, col] = find(M2 == 1);
 res = [row, col];
-accuracy2 = size(interset(res, gndtruth, 'rows'), 1)/size(gndtruth, 1);
+accuracy2 = size(intersect(res, gndtruth, 'rows'), 1)/size(gndtruth, 1);
 
 
 %% one example of final on-query
